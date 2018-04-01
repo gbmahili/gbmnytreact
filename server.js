e62ea9d32bb5f5452c0a1e1769e2511fdef3bd3c
gbmahili <georgesmahili@gmail.com>
2018-04-01 00:22:54 -0400

    Changed * routes to / route

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 // Create the express instance
 const app = express();
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "client/build")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // Get all the api routes saved inside of index.js
@@ -18,8 +18,9 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytreact";
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 // Any other request should be routed to this homepage:
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+app.get("*", (req, res) => {
+    // res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 })
 // Start the server
 app.listen(PORT, () => console.log(`App started at port ${PORT}!`));
