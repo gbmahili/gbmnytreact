a04465bbeffac4357e722872e53b2fb8303948c6
gbmahili <georgesmahili@gmail.com>
2018-03-31 23:13:18 -0400

    Added script to route all other requests to the homepage

--- server.js
+++ server.js
@@ -2,9 +2,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 // Create the express instance
 const app = express();
-app.use(express.static('public'));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // Get all the api routes saved inside of index.js
@@ -16,5 +17,9 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytreact";
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
+// Any other request should be routed to this homepage:
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+})
 // Start the server
 app.listen(PORT, () => console.log(`App started at port ${PORT}!`));
